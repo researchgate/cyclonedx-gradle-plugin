f574724e1287810d4a8aa1f945f6a18c1049e365
Dennis Schumann <hillkorn@gmail.com>
2018-12-17 22:14:27 +0100

    Generate bom for all dependencies including transitive.

--- src/main/groovy/org/cyclonedx/gradle/CycloneBomGeneratoinTask.groovy
+++ src/main/groovy/org/cyclonedx/gradle/CycloneBomGeneratoinTask.groovy
@@ -1,11 +1,14 @@
 package org.cyclonedx.gradle
 
+import com.github.packageurl.PackageURL
 import org.cyclonedx.BomGenerator
 import org.cyclonedx.BomParser
 import org.cyclonedx.model.Component
 import org.cyclonedx.util.BomUtils
 import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
+import org.gradle.api.artifacts.ModuleVersionIdentifier
+import org.gradle.api.artifacts.ResolvedArtifact
 import org.gradle.api.artifacts.ResolvedDependency
 import org.gradle.api.tasks.TaskAction
 
@@ -16,10 +19,11 @@ class CycloneBomGeneratoinTask extends DefaultTask {
 
     @TaskAction
     void cycloneBomGeneration() {
-        Set<ResolvedDependency> firstLevelModuleDependencies = project.configurations.getByName('runtime').resolvedConfiguration.firstLevelModuleDependencies
+        Set<ResolvedArtifact> firstLevelModuleArtifacts = project.configurations.getByName('runtime').resolvedConfiguration.resolvedArtifacts
 
         Set<Component> components = new HashSet<>()
-        firstLevelModuleDependencies.each { dep ->
+
+        firstLevelModuleArtifacts.each { dep ->
             components.add(convertToComponent(dep))
         }
 
@@ -43,13 +47,25 @@ class CycloneBomGeneratoinTask extends DefaultTask {
         }
     }
 
-    Component convertToComponent(ResolvedDependency dep) {
+    Component convertToComponent(ResolvedArtifact dep) {
         Component component = new Component()
-        component.setGroup(dep.getModuleGroup())
-        component.setName(dep.getName())
-        component.setVersion(dep.getModuleVersion())
+        ModuleVersionIdentifier moduleId = dep.getModuleVersion().id
+        component.setGroup(moduleId.group)
+        component.setName(moduleId.name)
+        component.setVersion(moduleId.version)
         component.setType("library")
-//        component.setHashes(BomUtils.calculateHashes(dep.getModuleArtifacts().first().file))
+        component.setHashes(BomUtils.calculateHashes(dep.file))
+        Map<String, String> qualifiers = new TreeMap<>()
+        if (dep.type != null) {
+            qualifiers.put('type', dep.type)
+        }
+        if (dep.classifier != null) {
+            qualifiers.put('classifier', dep.classifier)
+        }
+        if (dep.extension != null) {
+            qualifiers.put('extension', dep.extension)
+        }
+        component.setPurl(new PackageURL(PackageURL.StandardTypes.MAVEN, moduleId.group, moduleId.name, moduleId.version, qualifiers, null))
         return component
     }
 }
